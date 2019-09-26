cac74f33b7f53987807fb5f30d06816a85d7c129
liuhaikuan <liuhaikuan@game2sky.com>
2019-09-26 19:21:26 +0800

    练习

--- rust/ch08/src/main.rs
+++ rust/ch08/src/main.rs
@@ -1,6 +1,27 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = String::from("") + &s1 + &s2;
-    println!("s1: {}", s1);
+    let v = vec![1, 2, 3, 4, 8, 9, 12, 15];
+    println!("mean is {}", mean(&v));
+    println!("median is {}", median(&v));
+}
+
+fn mean(v: &Vec<i32>) -> f32 {
+    let mut sum: i32 = 0;
+    for i in v {
+        sum = sum + i;
+    }
+
+    sum as f32 / v.len() as f32
+}
+
+fn median(v: &Vec<i32>) -> i32 {
+    // 下标是从0开始的，所以最后要减去1.0
+    let mut middle = v.len() as f32 / 2.0 - 1.0;
+
+    println!("middle is {}", middle);
+
+    middle = middle.round();
+
+    println!("middle.round() is {}", middle);
+
+    v[middle as usize]
 }
