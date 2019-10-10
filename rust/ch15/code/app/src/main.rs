b85790ca36bc31d44b05ec59663891f8a06e5e8e
liuhaikuan <liuhaikuan@game2sky.com>
2019-10-10 19:04:55 +0800

    提交15章

---
+++ rust/ch15/code/app/src/main.rs
@@ -0,0 +1,41 @@
+use std::ops::Deref;
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping customSmartPointer with data `{}`!", self.data);
+    }
+}
